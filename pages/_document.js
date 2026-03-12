999f7dab27f14b1561b219fd5fa6ca1406c1dc1f
n14-py <luisfretes805@gmail.com>
2026-03-12 16:10:13 -0300

    monetag tranqui

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,25 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-        {/* Aquí es donde Next.js quiere que vayan las fuentes y estilos globales */}
+        {/* Fuentes y estilos globales */}
         <link 
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" 
         />
-        {/* Aquí puedes poner tus metadatos globales, favicon, etc. si faltan */}
+        
+        {/* --- MONETAG: VIGNETTE BANNER --- */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(s){s.dataset.zone='10721309',s.src='https://gizokraijaw.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+        }} />
+
+        {/* --- MONETAG: IN-PAGE PUSH --- */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(s){s.dataset.zone='10721319',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+        }} />
+
+        {/* --- MONETAG: PUSH NOTIFICATIONS --- */}
+        <script src="https://5gvci.com/act/files/tag.min.js?z=10721323" data-cfasync="false" async></script>
+
       </Head>
       <body>
         <Main />

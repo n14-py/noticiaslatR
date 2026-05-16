23c5c9b121fb4493fee001aeaa8a0c9c2d158363
n14-py <luisfretes805@gmail.com>
2026-05-16 01:44:26 -0300

    noticias gaming adsense

--- pages/_document.js
+++ pages/_document.js
@@ -10,18 +10,7 @@ export default function Document() {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" 
         />
         
-        {/* --- MONETAG: VIGNETTE BANNER --- */}
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(s){s.dataset.zone='10721309',s.src='https://gizokraijaw.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
-        }} />
 
-        {/* --- MONETAG: IN-PAGE PUSH --- */}
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(s){s.dataset.zone='10721319',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
-        }} />
-
-        {/* --- MONETAG: PUSH NOTIFICATIONS --- */}
-        <script src="https://5gvci.com/act/files/tag.min.js?z=10721323" data-cfasync="false" async></script>
 
       </Head>
       <body>
